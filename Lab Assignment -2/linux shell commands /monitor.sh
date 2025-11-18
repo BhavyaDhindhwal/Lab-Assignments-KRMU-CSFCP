35ead1e4e5e7811bda85a0ebf1ed8369b0fd8c67
BhavyaDhindhwal <bkdhindhwal143@gmail.com>
2025-11-18 19:41:27 +0530

    Update monitor.sh

--- Lab Assignment -2/linux shell commands /monitor.sh
+++ Lab Assignment -2/linux shell commands /monitor.sh
@@ -1,1 +1,20 @@
+# Script name: monior.sh
+# Purpose to monitor cpu and memory usage at regular intervals 
+# Author:Vasu Aggarwal
+# Date:10-11-25
 
+LOG_FILE="/home/vboxuser/monitor_log.txt"
+
+echo "system monitor started at $(date)" >> $LOG_FILE
+echo "---------------------------------" >> $LOG_FILE
+
+#run infinite loop (press ctrl+c to stop)
+
+while true
+do
+    echo "Time: $(date)" >> $LOG_FILE
+    echo "CPU and memory usage:" >> $LOG_FILE
+    top -b -n1 | head -5 >> $LOG_FILE
+    echo "--------------------------------" >> $LOG_FILE
+    sleep 5
+done
