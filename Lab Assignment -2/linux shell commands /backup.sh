db2fb512e6d2697d916a261bf33446c3ce46cc6e
BhavyaDhindhwal <bkdhindhwal143@gmail.com>
2025-11-18 19:36:36 +0530

    Update backup.sh

--- Lab Assignment -2/linux shell commands /backup.sh
+++ Lab Assignment -2/linux shell commands /backup.sh
@@ -1,1 +1,23 @@
+# Script name: backup.sh
+# Purpose: to take backup of a specified directory with timestamp
+# Made by:Bhavya
+# Date:10-11-25
 
+# source directory to backup
+SOURCE_DIR="/home/vboxuser/Documents"
+
+# destination directory for backups
+BACKUP_DIR="/home/vboxuser/Backups"
+
+# create backup directory if not exists 
+mkdir -p $BACKUP_DIR
+
+# create timestamp 
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# backup filename
+BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
+#create backup using tar
+tar -czf $BACKUP_FILE $SOURCE_DIR
+
+echo "Backup of $SOURCE_DIR completed successfully at $BACKUP_FILE"
